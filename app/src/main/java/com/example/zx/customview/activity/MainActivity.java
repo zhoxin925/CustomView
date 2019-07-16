7f90c10b22b56000c686417f84a4b8ea59766b16
zhoxin925 <2013268994@qq.com>
2019-07-16 13:23:58 +0800

    dev git

--- app/src/main/java/com/example/zx/customview/activity/MainActivity.java
+++ app/src/main/java/com/example/zx/customview/activity/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         context = this;
 
+        String ss1 = "chart change on master!";
     }
 
     @Override
