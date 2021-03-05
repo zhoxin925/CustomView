68698cd6b5fc121a1539f3111e7699f16ba264ab
zhoxin925 <2013268994@qq.com>
2021-03-05 17:38:23 +0800

    动画相关

--- app/src/main/java/com/example/zx/customview/activity/ObjectAnimActivity.java
+++ app/src/main/java/com/example/zx/customview/activity/ObjectAnimActivity.java
@@ -12,6 +12,16 @@ import android.widget.ImageView;
 import com.example.zx.customview.R;
 import com.example.zx.customview.bean.Point;
 
+/**
+ * 组合动画
+ *
+ * AnimatorSet.play(Animator anim)   ：播放当前动画
+ * AnimatorSet.after(long delay)   ：将现有动画延迟x毫秒后执行
+ * AnimatorSet.with(Animator anim)   ：将现有动画和传入的动画同时执行
+ * AnimatorSet.after(Animator anim)   ：将现有动画插入到传入的动画之后执行
+ * AnimatorSet.before(Animator anim) ：  将现有动画插入到传入的动画之前执行
+ */
+
 public class ObjectAnimActivity extends AppCompatActivity {
     private ImageView roundIv;
 
@@ -34,6 +44,8 @@ public class ObjectAnimActivity extends AppCompatActivity {
          * https://blog.csdn.net/carson_ho/article/details/72909894   属性动画
          *
          * ObjectAnimator类根据传入的属性名 去寻找 该对象对应属性名的 set（） & get（）方法，从而进行对象属性值的赋值
+         *
+         * with/height无效，因为其是设置的最大宽高---->继承原对象（ImageView）--setWidth--mTarget.getLayoutParams().width = width;
          */
         //ObjectAnimator animator = ObjectAnimator.ofFloat(roundIv, "alpha", 1f, 0f, 1f);
         ObjectAnimator animator = ObjectAnimator.ofFloat(roundIv, "rotation", 0, 360);//rotationX, rotationY
